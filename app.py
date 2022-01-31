d5cf77726bbdaacbd41ac8ed3383e440aab57927
janakirammeka <66859009+janakirammeka@users.noreply.github.com>
2022-01-30 23:17:12 -0800

    updated Celcius

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ def index():
         fahrenheit = ""
     return (
             """<form action="" method="get">
-                    Celsius temperature: <input type="text" name="celsius">
+                    Celsius temperature: <input type="text" name="Celsius">
                     <input type="submit" value="Convert to Fahrenheit">
                 </form>"""
             + "Fahrenheit: "
